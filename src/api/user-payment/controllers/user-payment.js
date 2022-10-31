72e53f91156920d80f3d09558f1b18fdf5d21937
Naveen Kumar <naveen@memorres.com>
2022-10-31 16:07:09 +0530

    Initial Commit

---
+++ src/api/user-payment/controllers/user-payment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-payment controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-payment.user-payment');
---
+++ src/api/user-payment/routes/user-payment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-payment router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-payment.user-payment');
